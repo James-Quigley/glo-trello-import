9d55bb0bca417663c768a9ac5b8fd518b76a2900
James Quigley <James.A.Quigley@asu.edu>
2018-08-04 00:23:02 -0700

    Styled the app a bit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,159 @@
 <template>
-  <div id="app">
-    <div id="trello">
-      <h2>Trello</h2>
-      <label for="apikey">API Key</label>
-      <input type="text" name="apikey" v-model="trelloApiKey">
-      <label for="token">Token</label>
-      <input type="text" name="token" v-model="trelloToken">
-      <button type="submit" v-on:click="loadTrello">Load Boards</button>
+    <!-- eslint-disable -->
+    <div id="app" class="">
 
-      <select id="boards" v-model="selectedBoard">
-        <option v-for="board of trelloBoards" :key="board.id" v-bind:value="board.id">
-          {{board.name}}
-        </option>
-      </select>
-    </div>
-    <div>
-      <button type="submit" v-on:click="importBoard">Import!</button>
-    </div>
-    <div id="glo">
-      <h2>Glo</h2>
-      <label for="glo-api-token">API Token</label>
-      <input type="text" name="glo-api-token" v-model="gloToken">
-    </div>
-    <div id="progress">
+        <md-toolbar class="md-primary">
+            <span class="md-title">Trello -> Glo Import</span>
+        </md-toolbar>
 
+        <div>
+
+            <div class="md-layout md-gutter md-alignment-center" style="min-height:55px;margin-top:0px;">
+                <div class="md-layout-item">
+                    <md-progress-bar id="progress" class="md-accent" md-mode="indeterminate" style="display:none"></md-progress-bar>
+                </div>
+            </div>
+
+            <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100 md-xsmall-size-100">
+                    <md-card class="md-primary">
+                        <md-card-header>
+                            <div class="md-title" style="color: #64dd17;">Trello Credentials</div>
+                        </md-card-header>
+
+                        <md-card-content>
+                            <p>You will need to get your API Key and Token from
+                                <a class="md-accent" href="https://trello.com/app-key">Trello</a>.</p>
+                            <div id="trello">
+                                <md-field>
+                                    <label>API Key</label>
+                                    <md-input v-model="trelloApiKey"></md-input>
+                                </md-field>
+                                <md-field>
+                                    <label>Token</label>
+                                    <md-input v-model="trelloToken"></md-input>
+                                </md-field>
+                            </div>
+                        </md-card-content>
+
+                        <md-card-actions>
+                            <md-button v-on:click="loadTrello" class="md-accent md-raised">Load Boards</md-button>
+                        </md-card-actions>
+                    </md-card>
+                </div>
+
+                <div class="md-layout-item md-small-size-100 md-xsmall-size-100">
+                    <md-card class="md-primary">
+                        <md-card-header>
+                            <div class="md-title" style="color: #64dd17;">Glo Credentials</div>
+                        </md-card-header>
+
+                        <md-card-content>
+                            <p>Your Glo Token can be found in the developer tools while on Glo. An official Public API should
+                                be out soon.</p>
+                            <div id="glo">
+                                <md-field>
+                                    <label>Board to Import</label>
+                                    <md-select v-model="selectedBoard">
+                                        <md-option v-for="board of trelloBoards" :key="board.id" v-bind:value="board.id">
+                                            {{board.name}}
+                                        </md-option>
+                                    </md-select>
+                                </md-field>
+                                <md-field>
+                                    <label for="glo-api-token">API Token</label>
+                                    <md-input v-model="gloToken"></md-input>
+                                </md-field>
+                            </div>
+                        </md-card-content>
+
+                        <md-card-actions>
+                            <md-button v-on:click="importBoard" class="md-accent md-raised">Import!</md-button>
+                        </md-card-actions>
+                    </md-card>
+                </div>
+            </div>
+
+
+
+
+            <div class="md-layout md-gutter">
+                <div class="md-layout-item">
+                    <md-card class="md-primary">
+                        <md-card-header>
+                            <div class="md-title" style="color: #64dd17;">Details</div>
+                        </md-card-header>
+                        <md-card-content>
+                            <div class="md-layout md-gutter">
+                                <div style="margin-top:5px;" class="md-layout-item md-xlarge-size-100 md-large-size-100 md-medium-size-100 md-small-size-100 md-xsmall-size-100">
+                                    <md-card>
+                                        <md-card-header>
+                                            <md-icon class="md-accent">transform</md-icon>
+                                            <span class="md-title"> Summary</span>
+                                        </md-card-header>
+                                        <md-card-content>
+                                            <p>This application will take an existing
+                                                <a class="md-accent" href="https://trello.com">Trello</a> board and import it into
+                                                <a class="md-accent" href="https://app.gitkraken.com/glo">GitKraken Glo</a>! Note that Glo is under active development, and therefore
+                                                not all parts of a Trello board may be copied 1:1.</p>
+                                        </md-card-content>
+                                    </md-card>
+                                </div>
+                                <div style="margin-top:5px;" class="md-layout-item md-xlarge-size-100 md-large-size-100 md-medium-size-100 md-small-size-100 md-xsmall-size-100">
+                                    <md-card>
+                                        <md-card-header>
+                                            <md-icon class="md-accent">sync_disabled</md-icon>
+                                            <span class="md-title"> One-Time Sync</span>
+                                        </md-card-header>
+                                        <md-card-content>
+                                            <p>This application is only a one-time import. This will not sync your items back
+                                                and forth between Trello and Glo.</p>
+                                        </md-card-content>
+                                    </md-card>
+                                </div>
+                                <div style="margin-top:5px;" class="md-layout-item md-xlarge-size-100 md-large-size-100 md-medium-size-100 md-small-size-100 md-xsmall-size-100">
+                                    <md-card>
+                                        <md-card-header>
+                                            <md-icon class="md-accent">security</md-icon>
+                                            <span class="md-title"> Security</span>
+                                        </md-card-header>
+                                        <md-card-content>
+                                            <p>We ask for your credentials for both services so that we can make API calls on
+                                                your behalf, automating the copy process. This application is entirely client
+                                                side, meaning the only time the credentials leave this page is when they
+                                                are used to make those API calls. Additionally, the code is open source on
+                                                <a class="md-accent" href="https://github.com/James-Quigley/glo-trello-import">GitHub</a>.</p>
+                                        </md-card-content>
+                                    </md-card>
+                                </div>
+                                <div style="margin-top:5px;" class="md-layout-item md-xlarge-size-100 md-large-size-100 md-medium-size-100 md-small-size-100 md-xsmall-size-100">
+                                    <md-card>
+                                        <md-card-header>
+                                            <md-icon class="md-accent">list</md-icon>
+                                            <span class="md-title"> Copied Items:</span>
+                                        </md-card-header>
+                                        <md-card-content>
+                                            <md-list>
+                                                <md-list-item>Board Name</md-list-item>
+                                                <md-list-item>Board Columns</md-list-item>
+                                                <md-list-item>Labels</md-list-item>
+                                                <md-list-item>Card Name</md-list-item>
+                                                <md-list-item>Card Description</md-list-item>
+                                                <md-list-item>Card Tasks</md-list-item>
+                                            </md-list>
+                                        </md-card-content>
+                                    </md-card>
+                                </div>
+                            </div>
+                        </md-card-content>
+                        <md-card-actions>
+
+                        </md-card-actions>
+                    </md-card>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -56,6 +184,7 @@ export default {
     components: {},
     methods: {
         loadTrello: function(event) {
+            document.getElementById("progress").style.display = "block";
             trelloAxios = axios.create({
                 baseURL: "https://api.trello.com/1",
                 params: {
@@ -68,12 +197,14 @@ export default {
                 .then(response => {
                     console.log(response);
                     this.trelloBoards = response.data;
+                    document.getElementById("progress").style.display = "none";
                 })
                 .catch(function(error) {
                     console.log(error);
                 });
         },
         importBoard: function(event) {
+            document.getElementById("progress").style.display = "block";
             console.log(this.selectedBoard);
             let promises = [
                 trelloAxios.get(`/boards/${this.selectedBoard}/cards`),
@@ -322,10 +453,20 @@ export default {
             }
 
             console.log("Added Descriptions!");
+            document.getElementById("progress").style.display = "none";
         }
     }
 };
 </script>
 
 <style lang="scss">
+#progress {
+    margin-top: 50px;
+}
+
+.md-layout.md-gutter {
+    margin-right: 0;
+    margin-left: 0;
+    margin-top: 50px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,35 @@
+/* eslint-disable */
+import {
+  MdButton,
+  MdCard,
+  MdField,
+  MdMenu,
+  MdList,
+  MdApp,
+  MdContent,
+  MdToolbar,
+  MdProgress,
+} from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/black-green-dark.css'
+// import 'vue-material/dist/theme/default-dark.css'
+
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
 Vue.config.productionTip = false;
 
+
+Vue.use(MdButton);
+Vue.use(MdCard);
+Vue.use(MdField);
+Vue.use(MdMenu);
+Vue.use(MdList);
+Vue.use(MdApp);
+Vue.use(MdContent);
+Vue.use(MdToolbar);
+Vue.use(MdProgress);
 new Vue({
   store,
   render: h => h(App),
