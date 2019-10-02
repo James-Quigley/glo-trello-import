d41c0d3050284730d0c7c50c6bc1f56633a838d1
James Quigley <james@quigley.us>
2019-10-02 09:12:28 -0400

    Switch to deployment on now

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production' ?
-        '/glo-trello-import/' : '/',
-    outputDir: path.resolve(__dirname, "./docs"),
     configureWebpack: {
         devtool: 'source-map',
     }
